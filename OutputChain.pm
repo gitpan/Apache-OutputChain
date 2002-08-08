efdb60312c2456a3732b0a0b1c54e8ddffa8e4e8
Jan Pazdziora <jpx-perl@adelton.com>
2002-08-08 17:44:04 +0000

    Import of JANPAZ/Apache-OutputChain-0.08 from CPAN.
    gitpan-cpan-distribution: Apache-OutputChain
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         JANPAZ/Apache-OutputChain-0.08.tar.gz
    gitpan-cpan-author:       JANPAZ
    gitpan-cpan-maturity:     released

--- OutputChain.pm
+++ OutputChain.pm
@@ -9,7 +9,7 @@ package Apache::OutputChain;
 use 5.004;
 use strict;
 use vars qw( $VERSION $DEBUG );
-$VERSION = 0.07;
+$VERSION = 0.08;
 
 use Apache::Constants ':common';
 $DEBUG = 0;
@@ -125,7 +125,7 @@ will call I<PRINT> method of the next class.
 
 =head1 VERSION
 
-0.07
+0.08
 
 =head1 AUTHOR
 
